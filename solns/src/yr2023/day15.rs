fc676028c9b038d68823b1ca3f20318ddeda1def
Daman Morris <damanm72@gmail.com>
2023-12-18 21:44:20 -0500

    Add tests for 2023.15

--- solns/src/yr2023/day15.rs
+++ solns/src/yr2023/day15.rs
@@ -74,15 +74,15 @@ struct Lens<'a> {
 mod tests {
     use super::*;
 
+    static INPUT: &str = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
+
     #[test]
-    #[ignore]
     fn part1() {
-        //assert_eq!((), Puzzle::part1(""));
+        assert_eq!(1320, Puzzle::part1(&INPUT));
     }
 
     #[test]
-    #[ignore]
     fn part2() {
-        //assert_eq!((), Puzzle::part2(""));
+        assert_eq!(145, Puzzle::part2(&INPUT));
     }
 }
